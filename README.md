ecd0740e99ffc7cdfaacc262801253b6f8b1b8d7
Thomas Chapeaux <thomas.chapeaux@sabca.be>
2016-11-14 11:36:16 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,13 @@
+# Barillet parser
+
+Parse a .csv file and produce a printable XML version of an improv 'barillet'
+
+## Usage
+
+"""
+	python parse_arbitre_csv.py <path to csv file>
+"""
+
+## Example
+
+Use the script on the given example then open the produced .xml file in a browser (tested on Chrome and Firefox)
---
+++ parse_arbitre_csv.py
@@ -0,0 +1,53 @@
+# coding: utf8
+
+import csv
+import sys
+from collections import namedtuple
+import xml.etree.ElementTree as ET
+import codecs
+
+Impro = namedtuple("Impro", ["nature", "titre", "nbj", "cate", "duree", "divers"])
+
+def get_impro_element(impro):
+    impro_xml = ET.Element("impro")
+    nature_xml = ET.SubElement(impro_xml, "nature")
+    nature_xml.text = impro.nature
+    titre_xml = ET.SubElement(impro_xml, "titre")
+    titre_xml.text = impro.titre
+    nbj_xml = ET.SubElement(impro_xml, "nbj")
+    nbj_xml.text = impro.nbj
+    cate_xml = ET.SubElement(impro_xml, "cate")
+    cate_xml.text = impro.cate
+    duree_xml = ET.SubElement(impro_xml, "duree")
+    duree_xml.text = impro.duree
+    divers_xml = ET.SubElement(impro_xml, "divers")
+    divers_xml.text = impro.divers
+    return impro_xml
+
+impros = []
+with codecs.open(sys.argv[1], "r", "utf-8") as f:
+    for line in [l.strip() for l in f.readlines() if len(l.strip()) > 0]:
+        line = line.split(";")
+        impros.append(Impro(    
+            nature=line[0].strip('"'),
+            titre=line[1].strip('"'),
+            nbj=line[2].strip('"'),
+            cate=line[3].strip('"'),
+            duree=line[4].strip('"'),
+            divers=" ".join(line[5:]),
+        ))
+
+with open("impro_out.xml", "w") as f:
+    barillet_xml = ET.Element("barillet")    
+    for impro in impros:
+        print "Parsed Impro: ", impro        
+        barillet_xml.append(get_impro_element(impro))
+    barillet_xml_str = ET.tostring(barillet_xml, encoding="UTF-8", method="xml")
+    # Quick&dirty: add css stylesheet to xml
+    barillet_xml_str = barillet_xml_str.replace(
+        "<?xml version='1.0' encoding='UTF-8'?>",
+        """<?xml version='1.0' encoding='UTF-8'?>\n<?xml-stylesheet href="impro_barillet.css"?>"""
+    )
+    f.write(barillet_xml_str)
+    f.write("\n")
+
